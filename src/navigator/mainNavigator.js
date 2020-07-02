93dc9265c295f2e52cb6501d2c9a138c3d743d31
Crowdbotics <team@crowdbotics.com>
2020-07-02 19:49:02 +0000

    remove package blueprint-react-native-message-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,7 +5,6 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
-import Messaging7684Navigator from '../features/Messaging7684/navigator';
 import BlankScreen47681Navigator from '../features/BlankScreen47681/navigator';
 import BlankScreen57680Navigator from '../features/BlankScreen57680/navigator';
 import BlankScreen47676Navigator from '../features/BlankScreen47676/navigator';
@@ -22,7 +21,6 @@ const AppNavigator = {
       screen: SplashScreen
     },
     //@BlueprintNavigationInsertion
-Messaging7684: { screen: Messaging7684Navigator },
 BlankScreen47681: { screen: BlankScreen47681Navigator },
 BlankScreen57680: { screen: BlankScreen57680Navigator },
 BlankScreen47676: { screen: BlankScreen47676Navigator },
