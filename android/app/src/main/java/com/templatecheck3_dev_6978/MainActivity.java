553b614a76b68bb5b327aade4c3a6de994adfeba
Crowdbotics <team@crowdbotics.com>
2020-07-02 19:40:01 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/templatecheck3_dev_6978/MainActivity.java
@@ -0,0 +1,15 @@
+package com.templatecheck3_dev_6978;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "templatecheck3_dev_6978";
+    }
+}
